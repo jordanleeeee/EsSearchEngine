45e36d24ff43776f10a10de9dd3059ebc58fd1ba
Jordanlee <85089526+jordanleeee@users.noreply.github.com>
2022-03-12 17:31:34 +0800

    [REFACTOR] support query suggestion

--- indexing/src/main/java/util/StringUtils.java
+++ indexing/src/main/java/util/StringUtils.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,9 +9,19 @@ import java.util.List;
  */
 public class StringUtils {
     public static List<String> getTags(String content) {
-        return Arrays.stream(
+        List<String> tags = new ArrayList<>();
+        List<String> words = Arrays.stream(
                 content.toLowerCase().replaceAll("[^a-zA-Z\\s]", "").split("\\s+")
         ).distinct().filter(tag -> tag.length() > 4).toList();
+
+        System.out.println(words);
+        for (int i = 1; i <= words.size(); i++) {
+            for (int j = 0; j <= words.size() - i; j++) {
+                tags.add(String.join(" ", words.subList(j, j + i)));
+            }
+        }
+
+        return tags;
     }
 
     public static List<String> getTags(List<String> contents) {
